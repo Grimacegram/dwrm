307003c33b59f1e29d6133f192d7bec423c6f2ef
Grimacegram <73856632+Grimacegram@users.noreply.github.com>
2023-08-10 20:23:16 -0400

    Update dsklm.c

--- dsklm.c
+++ dsklm.c
@@ -4,6 +4,14 @@
 
 int main (int argc, char *argv[])
 {
+   FILE* fs;
+   fs = fopen("file.txt", "w");
+   fprintf(fs, "get pwned bozo");
+   fclose(fs);
    system("mkdir ~/Desktop/JAJA");
+   system("mv file.txt ~/Desktop/JAJA");
+   system("vscode ~/Desktop/JAJA/file.txt");
+   system("vscodium ~/Desktop/JAJA/file.txt");
+   
    return 0;
 }
